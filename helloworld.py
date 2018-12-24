5b86cab12c693b0836c5473589e4668d174daa2a
shadowz667 <peteluke@gmail.com>
2018-12-23 23:09:03 -0500

    Created a function that maps the rgb colors in the board

--- helloworld.py
+++ helloworld.py
@@ -2,11 +2,23 @@ import sys
 import autopy
 print("Hello Atom")
 screen = autopy.bitmap.capture_screen(None)
-blue = autopy.bitmap.Bitmap.open('BejewledBot\\gems\\red.PNG')
-#loc = screen.find_every_bitmap(blue,.25)
-#print(loc)
+#Set location to top corner to start
+#These values are determined by screen size and window size
 x = 462.0+55.5
 y = 93.0+55.5
-autopy.mouse.move(x,y)
-autopy.mouse.move(x+111,y)
+
+
+def create_board():
+    board = []
+    for i in range(0,8):
+        row = []
+        for j in range(0,8):
+            row.append(screen.get_color(x+(j*111),y+(i*111)))
+        board.append(row)
+        print(row)
+    return board
+autopy.mouse.move(x+777,y+777)
+autopy.mouse.click()
+autopy.mouse.move(x+777,y+666)
+autopy.mouse.click()
 #print(autopy.mouse.location())
