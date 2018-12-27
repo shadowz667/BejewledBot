56277b4c410ae9a9359e355e19079a26496fd376
shadowz667 <peteluke@gmail.com>
2018-12-26 19:17:56 -0500

    updated finder to run on different game modes

--- finder.py
+++ finder.py
@@ -4,9 +4,18 @@ import time
 from PIL import Image
 import pytesseract
 # If you don't have tesseract executable in your PATH, include the following:
+#normal mode
+x1 = 130
+y1 = 170
+x2 = 190
+y2 = 50
+#zen mode
+x1,y1 = 137.0, 101.0
+x2,y2 = 309.0-x1, 157.0-y1
+
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 def get_score():
-    autopy.bitmap.capture_screen(((130,170),(190,50))).save("BejewledBot\\screencaps\\score.png","png")
+    autopy.bitmap.capture_screen(((x1,y1),(x2,y2))).save("BejewledBot\\screencaps\\score.png","png")
     score = pytesseract.image_to_string("BejewledBot\\screencaps\\score.png")
     if(score == ''):
         return 0
