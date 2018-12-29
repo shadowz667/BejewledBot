e81d31bfef96192389dbe03d406d779bd3ff666a
shadowz667 <peteluke@gmail.com>
2018-12-28 22:53:37 -0500

    Last commit before vacation

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # BejewledBot
 An exercise in machine learning. I hope to create something that learns how to play match 3 games
+
+Current version can play Bejewled and feed data into the models
+
+TODO:
+Update the score reader to be more accurate to reduce the number of instances with bad scoring
+Update the neural net to converge to a solution sooner (need help with this one)
+Create documentation on setting up the board position
+Devise a strategy for better board detection
+Test a categorical approach to data entry for models
--- model.py
+++ model.py
@@ -18,11 +18,12 @@ class BejewledBot():
         self.memory = deque(maxlen=2000)
         #under construction brain will be made smarter soon tm
         self.model = self._build_model()
+        self.score = 0
 
     def _build_model(self):
         model = Sequential()
         model.add(Flatten())
-        model.add(Dense(64))
+        model.add(Dense(224, activation='elu'))
         model.add(Dense(self.action_size))
         model.compile(optimizer='adam',
                       loss='logcosh',
@@ -31,14 +32,13 @@ class BejewledBot():
 
     def act(self, board):
         if np.random.rand() <= self.epsilon:
-            return random.randrange(256)
-        print("guessing.....")
+            return random.randrange(256), False
         act_values = self.model.predict(board)
-        return np.argmax(act_values)
+        return np.argmax(act_values), True
 
     #Need to look for more examples for calculating this going to use all values in memory as it is sparse
     def targetQ(self):
-        print("training")
+        print()
         for state, action, reward, next_state, done in self.memory:
             target = reward
             if not done:
@@ -49,13 +49,17 @@ class BejewledBot():
         if self.epsilon > self.ep_min:
             self.epsilon *= self.ep_dec
 
-    def find_move(self, action, board, lboard, score):
+    def find_move(self, action, lboard):
         dir = action%4
         gem = action//4
         x = gem%8
         y = gem//8
-        if game.check_move(x,y,dir,lboard) != True:
-            return board, lboard, -1, False
+        go = game.check_move(x,y,dir,lboard)
+        if go != True:
+            return -1, False
+        #print("Coordinates: " + str((x,y)))
+        #print("Direction: " + str(dir))
+        #print("Valid Move: " + str(go))
         if dir == 0:
             r = game.move_up(x,y)
         elif dir == 1:
@@ -65,54 +69,79 @@ class BejewledBot():
         else:
             r = game.move_right(x,y)
         if r==-1:
-            return board, lboard, r, False
-        #wait for move if applicable
+            return -1, False
+        #wait for move if applicable still cleaning this up
         time.sleep(1)
-        nscore = finder.get_score()
-        print("score: " + str(nscore))
+        nscore1 = finder.get_score()
+        nscore2 = 0
         trys = 0
-        while nscore%10!=0:
+        #wait for score to update if possible
+        while nscore1!=nscore2:
             time.sleep(1)
-            nscore = finder.get_score()
-            print("score: " + str(nscore))
+            nscore2 = nscore1
+            nscore1 = finder.get_score()
             trys += 1
             #giveup should eventually not need to do this but need to upgrade finder
-            if trys == 10:
-                nscore = 0
-        if nscore==score:
-            return board, lboard, -1, False
-        if nscore==0:
+            if trys == 20:
+                nscore1 = 0
+        if nscore1==self.score:
+            print("No Change Move")
+            return -1, False
+        if nscore1==0:
             #wait for level to transition/match if its real long
-            time.sleep(5)
-            nscore = finder.get_score()
+            time.sleep(10)
+            nscore1 = finder.get_score()
             #gameover
-            if nscore<=0:
+            if nscore1 <= 0:
                 print("gameover")
-                return board, lboard, -1, True
-        board, lboard = game.create_board()
-        return np.expand_dims(np.array(board),axis=0), lboard, (nscore-score)/50, False
-
+                return -1, True
+        if nscore1 > self.score:
+            reward = (nscore1-self.score)/10
+            self.score = nscore1
+            print("Reward: " + str(reward))
+            return reward, False
+        else:
+            #TODO understand when this is getting called, probably a problem with finder
+            #Temp solution set score to whatever is registering
+            print("Scores are: " + this.score + " New " + nscore1)
+            #reset score to get better metrics and give no points to prevent bad training
+            self.score = nscore1
+            return 0, False
     def run_game(self):
 
         for i in range(0,10):
             game.reset_game()
-            score = 0
+            self.score = 0
             board, lboard = game.create_board()
             board = np.expand_dims(np.array(board),axis=0)
             self.model.predict(board)
             runs = 0
             done = False
+            gflag = False
+            gs = 0
+            rm = 0
             while(done!=True):
                 runs += 1
-                action = self.act(board)
-                next_board, lboard, reward, done = self.find_move(action,board,lboard,score)
+                action, gflag = self.act(board)
+                reward, done = self.find_move(action,lboard)
+                next_board, lboard = game.create_board()
+                next_board = np.expand_dims(np.array(next_board),axis=0)
                 self.memory.append((board, action, reward, next_board, done))
                 board = next_board
-                if runs%50==0:
+                if reward > 0 and gflag:
+                    gs += 1
+                    print("Successful Guess: " + str(gs))
+                elif reward > 0:
+                    rm += 1
+                    print("Random Match: " + str(rm))
+                elif gflag:
+                    print("Bad Guess")
+                if runs%40==0:
+                    print("Training")
                     self.targetQ()
                 if runs>=10000:
                     done = True
-            targetQ()
+            self.targetQ()
 bot = BejewledBot()
 bot.run_game()
 #print(board)
